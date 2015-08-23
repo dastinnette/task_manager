f78805c4aacbf3db53f0a612e9237642ee27b249
David Stinnette <dastinnette@gmail.com>
2015-08-23 15:14:21 -0600

    Add task index route

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -2,6 +2,11 @@ class TaskManagerApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
 
   get '/' do
-    'hello, world!'
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = ["task1", "task2", "task3"]
+    erb :index
   end
 end
