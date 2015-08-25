ef6fe0a2e6e382b190eae523def7ac9d08b62230
David Stinnette <dastinnette@gmail.com>
2015-08-25 14:19:04 -0600

    Add delete method to controller

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -18,6 +18,11 @@ class TaskManagerApp < Sinatra::Base
     redirect "/tasks/#{id}"
   end
 
+  delete '/tasks/:id' do |id|
+    TaskManager.delete(id.to_i)
+    redirect '/tasks'
+  end
+
   get '/tasks' do
     @tasks = TaskManager.all
     erb :index
