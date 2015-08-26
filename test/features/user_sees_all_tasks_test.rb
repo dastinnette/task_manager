effe1bd6262c98d692494ab76ca0d4410052ccf5
David Stinnette <dastinnette@gmail.com>
2015-08-26 15:03:16 -0600

    Add final feature testing with Capybara

--- test/features/user_sees_all_tasks_test.rb
+++ test/features/user_sees_all_tasks_test.rb
@@ -1,7 +1,16 @@
 require_relative '../test_helper'
 
 class UserSeesAllTasksTest < FeatureTest
+
   def test_user_sees_index_of_tasks
-    asdf
+    visit "/"
+    assert page.has_content?("Welcome to Task Manager")
+  end
+
+  def test_frontpage_has_welcome_in_greeting
+    visit "/"
+    within("#greeting") do
+      assert page.has_content?("Welcome to Task Manager")
+    end
   end
 end
